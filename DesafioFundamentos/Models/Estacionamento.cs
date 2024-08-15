6ed152da4a1daa23f4868ff178beac3e1cfc4ada
MATHEUSFSSILVA <matheusfssilva@icloud.com>
2024-08-15 16:08:25 -0300

    Desafio concluído!

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -18,7 +18,7 @@ namespace DesafioFundamentos.Models
         public void AdicionarVeiculo()
         {
             // TODO: Pedir para o usuário digitar uma placa (ReadLine) e adicionar na lista "veiculos"
-            // *IMPLEMENTE AQUI*                       
+            // *IMPLEMENTADO*                     
             bool placaValida = false;
 
             while (!placaValida)
@@ -54,7 +54,7 @@ namespace DesafioFundamentos.Models
             Console.WriteLine("Digite a placa do veículo para remover:");
 
             // TODO: Pedir para o usuário digitar a placa e armazenar na variável placa
-            // *IMPLEMENTE AQUI*
+            // *IMPLEMENTADO*
             string placa = "";
             placa = Console.ReadLine().ToUpper();
 
@@ -65,12 +65,12 @@ namespace DesafioFundamentos.Models
 
                 // TODO: Pedir para o usuário digitar a quantidade de horas que o veículo permaneceu estacionado,
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
-                // *IMPLEMENTE AQUI*
+                // *IMPLEMENTADO*
                 int horas = int.Parse(Console.ReadLine());                    
                 decimal valorTotal = precoInicial + precoPorHora * horas;
 
                 // TODO: Remover a placa digitada da lista de veículos
-                // *IMPLEMENTE AQUI*
+                // *IMPLEMENTADO*
                 veiculos.Remove(placa); 
 
                 Console.WriteLine($"O veículo {placa} foi removido e o preço total foi de: R$ {valorTotal}");
@@ -88,7 +88,7 @@ namespace DesafioFundamentos.Models
             {
                 Console.WriteLine("Os veículos estacionados são:");
                 // TODO: Realizar um laço de repetição, exibindo os veículos estacionados
-                // *IMPLEMENTE AQUI*
+                // *IMPLEMENTADO*
                 for (int contador = 0; contador < veiculos.Count; contador++)
                 {
                     Console.WriteLine($"{contador+1} - {veiculos[contador]}");
